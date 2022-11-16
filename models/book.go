0aab79246ecad0a016acabd99f0f083602f06b1a
Juliano <julianofirme23@gmail.com>
2022-11-16 18:52:40 -0300

    feat: create the book model

---
+++ models/book.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Book struct {
+	ID          uint      `json:"id" gorm:"primarykey"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	MediumPrice string    `json:"medium_price"`
+	Author      string    `json:"author"`
+	ImageURL    string    `json:"image_url"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	DeletedAt   time.Time `gorm:"index" json:"deleted_at"`
+}
